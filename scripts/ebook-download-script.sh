7a3ab8f72018c17ecdea48afe28c117faddd7c4a
Rafael Carvalho <rafael.carvalho94@hotmail.com>
2024-01-18 23:19:01 +0000

    Add cronjob for ebook-convert

---
+++ scripts/ebook-download-script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+source $HOME/.bash_profile
+
+ebook-convert /recipes/expresso.recipe /import/expresso.epub --debug-pipeline debug --username $EXPRESSO_USERNAME --password $EXPRESSO_PASSWORD >> /var/log/cron.log 2>&1
